516e4a9dd9650f42c5223812e571db9f76ac52de
selva <selvakvs11@gmail.com>
2024-01-31 12:04:25 +0530

    profile model updated

--- backend/app/models/profile-model.js
+++ backend/app/models/profile-model.js
@@ -1,51 +1,3 @@
-// const {Schema,model} = require("mongoose")
-
-// const profileSchema = new Schema({
-    
-//     profilePic:String,
-//     description:String,
-//     userId:{
-//         type:Schema.Types.ObjectId,
-//         ref:"UserModel"
-//     },
-//     addressInfo: {
-//         address: String,
-//         city: String
-//     },
-//     location:{
-//         type:{
-//             type:String,
-//             required:true,
-//             enum:['Point']
-//         },
-//         coordinates: {      
-//             required:true,
-//             type:[Number]       //geospatial data
-//         }
-//     }, 
-//     bookings:[{
-//         type:Schema.Types.ObjectId,
-//         ref:"UserModel"
-//     }]
-
-// })
-
-// const ProfileModel = model("ProfileModel",profileSchema)
-
-// module.exports = ProfileModel
-
-
-
-
-
-// // # Profile
-// //     
-// //     
-// //     
-// //      
-// //      - { lat, lng }
-// //     : []
-
 const {Schema,model} = require("mongoose")
 
 const profileSchema = new Schema({
@@ -56,14 +8,19 @@ const profileSchema = new Schema({
         type:Schema.Types.ObjectId,
         ref:"UserModel"
     },
-    geo :{
-        latitude:Number,
-        longitude:Number
+    addressInfo: {
+        address: String,
+        city: String
     },
-    address:[{
-        type:String,
-        required:true
-    }],
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'] //Point
+        },
+        coordinates: {
+            type: [Number] // Geospatial data for 2d sphere
+        }
+    },
     bookings:[{
         type:Schema.Types.ObjectId,
         ref:"UserModel"
